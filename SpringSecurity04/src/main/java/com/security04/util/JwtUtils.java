c56b0e1bbefadb05cf85eb0908c8c6278f77fa5d
jsan2321 <jsantiago.c@hotmail.com>
2024-09-01 22:41:06 -0500

    created token

--- SpringSecurity04/src/main/java/com/security04/util/JwtUtils.java
+++ SpringSecurity04/src/main/java/com/security04/util/JwtUtils.java
@@ -1,4 +1,77 @@
 package com.security04.util;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
 public class JwtUtils {
+
+    @Value("${security.jwt.key.private}") // from a sha generator key
+    private String privateKey; // only backend knows it
+
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
+
+    public String createToken(Authentication authentication) {
+        Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+        String username = authentication.getPrincipal().toString();
+        String authorities = authentication.getAuthorities()
+                .stream()
+                //.map(grantedAuthority -> grantedAuthority.getAuthority())
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(",")); // get all authorizations in a String, separating each one with a comma
+
+        String jwtToken = JWT.create()
+                .withIssuer(this.userGenerator) // user which generates token
+                .withSubject(username) // who is token generated to... authenticated user
+                .withClaim("authorities", authorities)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1800000))
+                .withJWTId(UUID.randomUUID().toString()) // assign random ID to token
+                .withNotBefore(new Date(System.currentTimeMillis())) // from what moment is it valid?
+                .sign(algorithm);
+        return jwtToken;
+    }
+
+    public DecodedJWT validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(this.userGenerator)
+                    .build();
+
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT;
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException("Token invalid, not Authorized");
+        }
+    }
+
+    public String extractUsername(DecodedJWT decodedJWT){
+        return decodedJWT.getSubject().toString(); // get subject (user) from token
+    }
+
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName) {
+        return decodedJWT.getClaim(claimName);
+    }
+
+    public Map<String, Claim> returnAllClaims(DecodedJWT decodedJWT) {
+        return decodedJWT.getClaims();
+    }
+
 }
